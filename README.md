f9432e337c0078d79b0f04521864f9843fdf1752
hanjinhui638 <jh312jh@naver.com>
2019-11-08 11:03:04 +0900

    2019-11-08

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Spring_3
+
+2. 세팅 
+	- Encoding 설정 
+	- API(Library) 구성 pom.xml
  
---
+++ src/main/java/com/jh/s3/dao/board/NoticeDAO.java
@@ -0,0 +1,9 @@
+package com.jh.s3.dao.board;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NoticeDAO {
+	
+
+}
